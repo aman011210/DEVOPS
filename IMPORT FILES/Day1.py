45eb04c77f08b41d963c66e6eb0c80bc6385a507
Amanul Haque <amanullhakhan0112101@gmail.com>
2025-10-23 23:27:49 +0530

    Added new folder

---
+++ IMPORT FILES/Day1.py
@@ -0,0 +1,32 @@
+# 🗓️ Day 1 – Basics & Fundamentals
+
+# ⏱ Python (10 min)
+
+# Data types: int, float, string, list, tuple, dict, set
+
+# Type casting, input/output, operators
+
+# 💻 Practice:
+
+# Write a script to swap two numbers without using a temp variable.
+num1=10
+num2=5
+num1=num2+num1
+num2=num1-num2
+num1=num1-num2
+
+
+print(num1)
+print(num2)
+
+
+str ="PythonForDevOps"
+print(str.lowercase())
+
+# Create a dictionary with student marks and find the average.
+
+# 🐧 Linux (10 min)
+
+# Basic navigation: pwd, cd, ls, cat, touch, mkdir, rm
+
+# File viewing & redirection: >, >>, cat, less, more
